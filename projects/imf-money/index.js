ad0eb2edb15c45d42dffad8e2ddcc50fa079073b
miffybunnyrabbit <miffybunnyrabbit@gmail.com>
2024-08-02 17:56:28 +1000

    Update index.js

--- projects/imf-money/index.js
+++ projects/imf-money/index.js
@@ -3,7 +3,7 @@ const PEPE_TOKEN_CONTRACT = '0x6982508145454Ce325dDbE47a25d4ec3d2311933';
 const LOCKED_MONEY_CONTRACT = '0x30F75834cB406b7093208Fda7F689938aCBD1EeB'; //wallet that has all the locked money
 
 module.exports = {
-  methodology: 'Sums the value of deposited memes',
+  methodology: 'Counts all the tokens being used as collateral of CDPs.',
   ethereum: {
     tvl: sumTokensExport({ owner: LOCKED_MONEY_CONTRACT, token: PEPE_TOKEN_CONTRACT}),
   }
